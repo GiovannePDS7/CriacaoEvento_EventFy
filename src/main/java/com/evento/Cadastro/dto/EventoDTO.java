310eeeb121c9ff0a0d905689864e03a6727c4f02
Giovanne Pagano <giovanne3282@gmail.com>
2024-11-29 19:59:40 -0300

    config banco

--- src/main/java/com/evento/Cadastro/dto/EventoDTO.java
+++ src/main/java/com/evento/Cadastro/dto/EventoDTO.java
@@ -1,6 +1,7 @@
 package com.evento.Cadastro.dto;
 
 import com.evento.Cadastro.entity.Organizador;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -14,8 +15,11 @@ import java.time.LocalTime;
 public class EventoDTO {
 
     private String nomeEvento;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dataEvento;
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime horarioInicio;
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime horarioFim;
     private String localEvento;
     private String tipoEvento;
